e5eba5517602f87310a1a1f8c0c840c436378f0a
nikita7757 <132663150+nikita7757@users.noreply.github.com>
2023-09-11 22:18:09 +0530

    Create d1.cpp

---
+++ d1.cpp
@@ -0,0 +1,13 @@
+//Addition of two numbers without using third variable
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter value of a:";
+    cin>>a;
+    cout<<"enter value of b:";
+    cin>>b;
+    cout<<"addition of "<<a<<" and "<<b<<" is "<<a+b;
+    return 0;
+}
